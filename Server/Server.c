975d43ccc5159edc039d2a00d074bab6f0399f8d
Marijus Laucevicius <gringolithuania@gmail.com>
2018-04-11 17:15:12 +0300

    Server + Client

---
+++ Server/Server.c
@@ -0,0 +1,49 @@
+﻿/*
+Bind socket to port 8888 on localhost
+*/
+
+#include<stdio.h>
+#include<winsock2.h>
+
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsa;
+	SOCKET s;
+	struct sockaddr_in server;
+
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		return 1;
+	}
+
+	printf("Initialised.\n");
+
+	//Create a socket
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+	{
+		printf("Could not create socket : %d", WSAGetLastError());
+	}
+
+	printf("Socket created.\n");
+
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(8888);
+
+	//Bind
+	if (bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+	{
+		printf("Bind failed with error code : %d", WSAGetLastError());
+	}
+
+	puts("Bind done");
+
+	closesocket(s);
+
+	return 0;
+}
